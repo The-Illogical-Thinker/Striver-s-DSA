3ff7ad4a7d261ea01c1aa6481c03d547cdb827a5
The-Illogical-Thinker <tanmoybortamuli@gmail.com>
2026-06-15 12:31:05 +0530

    Initial commit

---
+++ Learn The Basics/Things to know/practice.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+    cout << "Value of x = " << x << endl << "Value of y = " << y << endl;
+    return 0;
+}
